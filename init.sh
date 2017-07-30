2e5b9fec6f58c436b3e998faac45410f9eb86a17
qwerty222 <drzvladimir@rambler.ru>
2017-07-30 18:59:22 +0300

    create file structure for project

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+mkdir web
+mkdir web/public
+mkdir web/uploads
+mkdir web/etc
+mkdir web/public/img
+mkdir web/public/css
+mkdir web/public/js
+
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
