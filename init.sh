4ba4c1e0e92f62703daeffdea479ff0f34ae025d
qwerty222 <drzvladimir@rambler.ru>
2017-07-31 14:40:52 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,3 @@
-mkdir web
-mkdir web/public
-mkdir web/uploads
-mkdir web/etc
-mkdir web/public/img
-mkdir web/public/css
-mkdir web/public/js
-
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
