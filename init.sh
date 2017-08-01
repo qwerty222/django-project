83aaa3f6eca8889f9aa5e2b66be090dba8491287
qwerty222 <drzvladimir@rambler.ru>
2017-08-01 23:22:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo /etc/init.d/gunicorn restart
