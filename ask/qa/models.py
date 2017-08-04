5ae35bbf7cc212bc9882ad2379409e909bddf782
qwerty222 <drzvladimir@rambler.ru>
2017-08-04 22:51:07 +0300

    add models Question and Answer

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,5 +2,39 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# - вопрос
+class Question(models.Model):
+# - заголовок вопроса
+    title = models.CharField(max_length=255)
+# - полный текст вопроса
+    text = models.TextField()
+# - дата добавления вопроса
+    added_at = models.DateField()
+# - рейтинг вопроса (число)
+    rating = models.IntegerField()
+# - автор вопроса
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+# - список пользователей, поставивших "лайк"
+    likes = models.ManyToManyField(User)
+
+# - менеджер модели Question
+class QuestionManager():
+# - метод возвращающий последние добавленные вопросы
+    def new():
+        return Question.objects.order_by('-added_at')[:5]
+# - метод возвращающий вопросы отсортированные по рейтингу
+    def popular():
+        return Question.objects.order_by('-rating')
+
+# - ответ
+class Answer(models.Model):
+# - текст ответа
+    text = models.TextField()
+# - дата добавления ответа
+    added_at = models.DateField()
+# - вопрос, к которому относится ответ
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+# - автор ответа
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ initdb.sh
@@ -0,0 +1,7 @@
+sudo /etc/init.d/mysql start
+
+mysql -uroot -e "CREATE DATABASE qa"
+mysql -uroot -e "CREATE USER 'django'@'localhost' IDENTIFIED BY 'pass123';
+				 GRANT ALL ON qa.* TO 'django'@'localhost';"
+
+sudo python /home/box/web/ask/manage.py syncdb
